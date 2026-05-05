89a9fe91ffff9ea4add7b23b076d39cecb1b2427
Xenon5401 <boursicotetann41@gmail.com>
2026-05-05 11:55:10 +0200

    api add base

---
+++ Backend/src/main/java/bts_ciel/local/ldap_api/LdapApiApplication.java
@@ -0,0 +1,13 @@
+package bts_ciel.local.ldap_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/bts_ciel/local/ldap_api/LdapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bts_ciel.local.ldap_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
